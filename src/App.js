afc1e83194f4c8e3c7d0dbc4f7b91e94e783a8fd
Johannes <johannes@lotze.xyz>
2023-12-24 12:06:07 +0100

    Added Delete Function

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import React, { useState } from "react";
 import { marked } from "marked";
 
-function ExpandingDiv({ heading, markdownText }) {
+function ExpandingDiv({ heading, markdownText, onDelete}) {
   const [isExpanded, setIsExpanded] = useState(false);
 
   // Render the first few words as a preview
@@ -28,7 +28,12 @@ function ExpandingDiv({ heading, markdownText }) {
           dangerouslySetInnerHTML={getMarkdownHtml(markdownText)}
         />
       ) : (
-        <p>{previewText}</p>
+        <>
+          <button className="delete-btn" onClick={onDelete}>
+            Delete
+          </button>
+          <p>{previewText}</p>
+        </>
       )}
       {isExpanded && (
         <span className="close-btn" onClick={handleClick}>
@@ -40,14 +45,18 @@ function ExpandingDiv({ heading, markdownText }) {
 }
 
 function App() {
-  const data = [
+  const [data, setData] = useState( [
     {
       heading: "Heading 1",
       markdownText:
         "Dear Ethics Department Team, \n\nI hope this message finds you well. I am writing to express some concerns that have recently come to my attention. It has been observed that the department's output has not been aligning with the established goals, a situation that is causing significant dissatisfaction among our shareholders. This misalignment not only impacts our department's performance but also affects the company's reputation and stakeholder confidence. I would appreciate it if we could urgently address these issues, ensuring our practices align with both our internal standards and shareholder expectations. Your immediate attention and action in improving our output and meeting our goals are crucial. \n\nThank you for your understanding and cooperation in this matter. \n\nBest regards,\nJohannes",
     },
     // ... other data entries ...
-  ];
+  ]);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
 
   return (
     <div className="App">
@@ -55,9 +64,10 @@ function App() {
       <div className="mailBody">
         {data.map((item, index) => (
           <ExpandingDiv
-            key={index}
+            key={item.id}
             heading={item.heading}
             markdownText={item.markdownText}
+            onDelete={() => handleDelete(item.id)}
           />
         ))}
       </div>
